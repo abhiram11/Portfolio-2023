25b5d0d98fbd418d484b1bba701f7acb8738ecac
abhiram11 <abhiramsatpute@gmail.com>
2023-03-09 11:19:50 +0530

    init vite react template code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Yey</h1>
+    </div>
+  )
+}
+
+export default App
